ba250bfab5a763a9b221e27c1725cb168f8893c2
MaxKiv <maxkivits42@gmail.com>
2025-12-02 16:31:36 +0100

    feat(tb6600): hardware pwm + maxon draft

--- firmware/stm32h753/tb6600/src/lib.rs
+++ firmware/stm32h753/tb6600/src/lib.rs
@@ -1,15 +1,28 @@
 #![no_std]
 
 use defmt::trace;
+use embassy_stm32::{
+    time::Hertz,
+    timer::{GeneralInstance4Channel, simple_pwm::SimplePwm},
+};
 use embedded_hal::digital::OutputPin;
 use embedded_hal_async::delay::DelayNs;
 use thiserror::Error;
 
-pub struct Tb6600<StepPin, DirPin, Delay> {
-    step_pin: StepPin,
+const BASE_DUTY_CYCLE_PERCENT: u8 = 50;
+pub const BASE_STEP_FREQUENCY_HZ: Hertz = Hertz(10_000);
+pub const MIN_STEP_FREQUENCY_HZ: Hertz = Hertz(1);
+pub const MAX_STEP_FREQUENCY_HZ: Hertz = Hertz(25_000); // 100khz max theoretical (~5µs pulse width at 50% dc), but this cooks the Tb6600 already
+
+pub struct Tb6600<Timer, DirPin, Delay>
+where
+    Timer: GeneralInstance4Channel, // General-purpose 16-bit timer with 4 channels instance
+{
+    name: &'static str,
+    step_pwm: SimplePwm<'static, Timer>,
+    step_frequency: Hertz,
     dir_pin: DirPin,
-    // enable: Option<EnablePin>,
-    // enabled: bool,
+    enabled: bool,
     delay: Delay,
     /// Fierce googling: min pulse width ~5µs, maybe?
     pub direction: Direction,
@@ -32,29 +45,77 @@ pub enum Direction {
     Reverse,
 }
 
-impl<StepPin, DirPin, Delay> Tb6600<StepPin, DirPin, Delay>
+impl<Timer, DirPin, Delay> Tb6600<Timer, DirPin, Delay>
 where
-    StepPin: OutputPin,
-    DirPin: OutputPin,
-    Delay: DelayNs,
+    DirPin: OutputPin,              // Any output pin
+    Delay: DelayNs,                 // Nanosecond capable delay
+    Timer: GeneralInstance4Channel, // General-purpose 16-bit timer with 4 channels instance to back the pwm impl
 {
-    pub fn new(step_pin: StepPin, dir_pin: DirPin, delay: Delay, pulse_period_us: u32) -> Self {
-        Self {
-            step_pin,
+    pub fn new(
+        name: &'static str,
+        mut step_pwm: SimplePwm<'static, Timer>,
+        dir_pin: DirPin,
+        delay: Delay,
+        pulse_period_us: u32,
+    ) -> Self {
+        step_pwm.ch1().disable(); // start disabled
+
+        let mut out = Self {
+            name,
+            step_pwm,
+            step_frequency: BASE_STEP_FREQUENCY_HZ,
             dir_pin,
             delay,
             direction: Direction::Forward,
             pulse_period_us,
-        }
+            enabled: false,
+        };
+
+        out.set_speed(BASE_STEP_FREQUENCY_HZ.0);
+
+        out
     }
 
-    // ENABLE LOW = ON + EN tied low -> Driver is always on
-    pub fn enable(&mut self) {}
+    /// ENABLE LOW = ON + EN tied low -> Driver is always on
+    /// Software enables the step pwm output
+    pub fn start_stepping(&mut self) {
+        self.control_stepping(true);
+    }
 
-    // ENABLE LOW = ON + EN tied low -> Driver is always on
-    pub fn disable(&mut self) {}
+    /// ENABLE LOW = ON + EN tied low -> Driver is always on
+    /// Software disables the step pwm output
+    pub fn stop_stepping(&mut self) {
+        self.control_stepping(false);
+    }
 
-    // Set step direction
+    /// ENABLE LOW = ON + EN tied low -> Driver is always on
+    /// Software controls the step pwm output
+    pub fn control_stepping(&mut self, should_step: bool) {
+        if should_step {
+            trace!(
+                "{} started stepping at {}hz",
+                self.name, self.step_frequency
+            );
+            self.step_pwm.ch1().enable();
+        } else {
+            trace!("{} stopped stepping", self.name);
+            self.step_pwm.ch1().disable();
+        }
+        self.enabled = should_step;
+    }
+
+    /// Set stepper speed
+    pub fn set_speed(&mut self, frequency: u32) {
+        let frequency = frequency.clamp(MIN_STEP_FREQUENCY_HZ.0, MAX_STEP_FREQUENCY_HZ.0);
+        trace!("{} set speed to {}Hz", self.name, frequency);
+        self.step_pwm.set_frequency(Hertz(frequency));
+        self.step_pwm
+            .ch1()
+            .set_duty_cycle_percent(BASE_DUTY_CYCLE_PERCENT); // set_frequency docs suggests I have to call this again
+        self.step_frequency = Hertz(frequency);
+    }
+
+    /// Set step direction
     pub async fn set_direction(&mut self, direction: Direction) -> Result<(), TB6600Error> {
         self.direction = direction;
 
@@ -75,6 +136,7 @@ where
         Ok(())
     }
 
+    /// Flip step direction
     pub async fn flip_direction(&mut self) -> Result<(), TB6600Error> {
         let dir = match self.direction {
             Direction::Forward => Direction::Reverse,
@@ -86,41 +148,10 @@ where
         Ok(())
     }
 
-    // Perform a single step
-    pub async fn step_once(&mut self) -> Result<(), TB6600Error> {
-        trace!("Stepper stepping once");
-
-        self.step_pin.set_high().map_err(|_| TB6600Error::StepPin)?;
-
-        self.delay.delay_us(50).await;
-
-        self.step_pin.set_low().map_err(|_| TB6600Error::StepPin)?;
-
-        self.delay.delay_us(self.pulse_period_us).await;
-
-        Ok(())
-    }
-
-    pub async fn step_n(&mut self, steps: u32) -> Result<(), TB6600Error> {
-        for _ in 0..steps {
-            self.step_once().await.map_err(|_| TB6600Error::StepPin)?;
-        }
-
-        Ok(())
-    }
-
-    // Perform a single step with given period
-    pub async fn step_once_with_period(&mut self, pulse_period_us: u32) -> Result<(), TB6600Error> {
-        trace!("Stepper stepping once");
-
-        self.step_pin.set_high().map_err(|_| TB6600Error::StepPin)?;
-
-        self.delay.delay_us(50).await;
-
-        self.step_pin.set_low().map_err(|_| TB6600Error::StepPin)?;
-
-        self.delay.delay_us(pulse_period_us).await;
-
-        Ok(())
+    /// Set step pwm dc
+    /// Percentage [0-100]
+    pub fn set_duty_cycle_percent(&mut self, percentage: u8) {
+        trace!("{} set dc to {}%", self.name, percentage);
+        self.step_pwm.ch1().set_duty_cycle_percent(percentage);
     }
 }
